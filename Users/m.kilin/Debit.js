31f13d113578c146ab541b965c93592fb2ca4e67
massita <massita@mail.ru>
2017-11-07 20:25:41 +0300

    Premum

--- Users/m.kilin/Debit.js
+++ Users/m.kilin/Debit.js
@@ -1,63 +1,82 @@
 function DebitCard(nPIN) {
 
+    const OK = 0;
+    const NOTAUTH = -1;
+    const NOTMONEY = -2;
+
     var _PIN = nPIN;
     var _balance = 0;
     var _auth = false;
+    this.status = OK;
+
+    //var obj = this;
+
     this.auth = function (uPIN) {
         if (uPIN == _PIN) {
             _auth = true;
-            console.log('Authentification succesful');
+            this.status = OK;
         }
         else  
         {
-            console.log('Authentification Failed');
+            this.status = NOTAUTH;
         }
+        return this.status
     };
     
-    this.putMoney = function (value) {
+    this.putMoney = function (value) { 
         if (_auth) {
         _balance += value;
-        console.log('You put: ' + value);
+        this.status = OK;
         }
         else {
-            console.log("You are not authorized. Please make auth");
+            _status = NOTAUTH;
         }
+        return this.status
     };
 
     this.checkMoney = function () {
         if (_auth) {
-            console.log('Your balance: ' + _balance);
+            this.status = _balance;
         }
         else {
-            console.log("You are not authorized. Please make auth");
+            this.status = NOTAUTH;
         }
+        return this.status
     }
     this.getMoney = function (value) {
         if ((_auth) && (_balance >= value)) {
             _balance -= value;
-            console.log('You get: ' + value);
+            this.status = OK;
         }
         else if (_auth) {
-            console.log("You have not enought money");
+            this.status = NOTAUTH;
         }
         else {
-            console.log("YYou are not authorized. Please make auth");
+            this.status = NOTMONEY;
         }
+        return this.status;
     };
 
     
 };
-card = new DebitCard(5555);
-card.checkMoney();
-card.putMoney();
-card.getMoney();
-card.auth(4444);
-card.auth(5555);
-card.checkMoney();
-card.getMoney(400);
-card.putMoney(400);
-card.checkMoney();
-card.getMoney(500);
-card.getMoney(300);
-card.checkMoney();
+
+function Premium(nPIN) {
+    var _percent = 0.1;
+    DebitCard.call(this,nPIN);
+    var _pGetMoney = this.getMoney;
+    this.getMoney = function(amount) {
+        _pGetMoney.call(this,amount);
+        this.putMoney(amount*_percent);
+        return this.status
+    }
+};
+card = new Premium(5555);
+card2 = new DebitCard(4444);
+console.log(card.checkMoney()); 
+console.log(card.auth(5555));
+console.log(card.putMoney(1000));
+console.log(card.checkMoney());
+console.log(card.getMoney(500));
+console.log(card.checkMoney());
+
 
